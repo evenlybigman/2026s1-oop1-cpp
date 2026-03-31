3d76a9d28393c9d26f377c9bf0770ed1d03bdcb4
Eve <evenlybigman@gmail.com>
2026-03-31 15:25:52 +0900

    객체 배열 실습

---
+++ week05_array/array.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+class Circle {
+	int radius = 1;
+public:
+	Circle() { radius = 1; }
+	Circle(int r) { radius = r; }
+	double getArea();
+};
+
+double Circle::getArea() {
+	return 3.14 * radius * radius;
+}
+
+
+int main() {
+	Circle Circles[3] = { Circle(1),Circle(5), Circle() };
+	for (int i = 0; i < 3; i++) {
+		cout << Circles[i].getArea() << "\t";
+	}
+	return;
+}
