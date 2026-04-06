a3c0fd85d22623dc7e1c563dbdca918d2b5ff6f0
Eve <evenlybigman@gmail.com>
2026-04-06 18:07:36 +0900

    swap 함수 실습 추가

---
+++ week06_swap/swap.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+void swap(int* swapA, int* swapB) {
+	int tmp;
+	tmp = *swapA;
+	*swapA = *swapB;
+	*swapB = tmp;
+}
+
+int main() {
+	int m, n;
+
+	cout << "A의 값 입력: ";
+	cin >> m;
+	cout << "B의 값 입력: ";
+	cin >> n;
+	swap(&m, &n);
+	cout << m << "\t" << n << endl;
+	return 0;
+}
