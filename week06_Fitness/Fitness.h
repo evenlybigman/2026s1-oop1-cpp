38e67801e2f4371d263ecf1db27d881cff6b1f78
Eve <evenlybigman@gmail.com>
2026-04-07 20:54:42 +0900

    과제 5-8 추가

---
+++ week06_Fitness/Fitness.h
@@ -0,0 +1,42 @@
+﻿#ifndef FITNESS_H
+#define
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Fitness {
+	string man = "남";
+	string woman = "여";
+	int countMan = 0;
+	int countWoman = 0;
+public:
+	Fitness& arrive(string gender, int count);
+	int& at(string gender);
+	void count(string gender, int& count);
+};
+
+
+Fitness& Fitness::arrive(string gender, int count) {
+	if (gender == man)
+		countMan += count;
+	else if (gender == woman)
+		countWoman += count;
+	return *this;
+}
+
+void Fitness::count(string gender, int& count) {
+	if (gender == man)
+		count = countMan;
+	else if (gender == woman)
+		count = countWoman;
+}
+
+int& Fitness::at(string gender) {
+	if (gender == man)
+		return countMan;
+	else
+		return countWoman;
+}
+
+#endif
---
+++ week06_Fitness/main.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Fitness {
+	string man = "남";
+	string woman = "여";
+	int countMan = 0;
+	int countWoman = 0;
+public:
+	Fitness& arrive(string gender, int count);
+	int& at(string gender);
+	void count(string gender, int& count);
+};
+
+
+Fitness& Fitness::arrive(string gender, int count) {
+	if (gender == man)
+		countMan += count;
+	else if (gender == woman)
+		countWoman += count;
+	return *this;
+}
+
+void Fitness::count(string gender, int& count) {
+	if (gender == man)
+		count = countMan;
+	else if(gender == woman)
+		count = countWoman;
+}
+
+int& Fitness::at(string gender) {
+	if (gender == man)
+		return countMan;
+	else 
+		return countWoman;
+}
+
+int main() {
+	Fitness altong;
+	altong.arrive("남", 3).arrive("여", 6).arrive("남", 9);
+
+
+	int m = 0, w = 0;
+	altong.count("남", m); //m에 남자 회원수 불러오기
+	altong.count("여", w); //w에 여자 회원수 불러오기
+	cout << "남자 회원 " << m << "명, 여자 회원 " << w << "명" << endl;
+
+	altong.at("남") = 100; //남자 회원수를 100으로 저장
+	m = altong.at("남"); //남자 회원수를 m에 저장
+	w = altong.at("여"); // 여자 회원수를 w에 저장
+	cout << "남자 회원 " << m << "명, 여자 회원 " << w << "명" << endl;
+}
