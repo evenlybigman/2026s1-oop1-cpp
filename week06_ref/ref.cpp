6238c0d433e1cf044ab96c2f0882a81756b28e6a
Eve <evenlybigman@gmail.com>
2026-04-07 16:09:58 +0900

    실습 추가

---
+++ week06_ref/ref.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int m = 10;
+	int n = 5;
+	int* p = &n;
+	int& ref = n;
+	p = &ref;
+	ref = m;
+	cout << "n의 값: " << n << "\tref의 값: " << ref << "\tp의 값: " << *p << endl;
+	ref++;
+	cout << "ref 값 증가" << endl;
+	cout << "n의 값: " << n << "\tref의 값: " << ref << "\tp의 값: " << *p << endl;
+}
