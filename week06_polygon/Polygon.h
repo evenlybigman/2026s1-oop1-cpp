90c825bedc1286b9a785a401c858371e0a58758b
Eve (Juhyeok Kim) <juhyeok.works@gmail.com>
2026-04-10 20:33:24 +0900

    polygon.h 주석 추가

--- week06_polygon/Polygon.h
+++ week06_polygon/Polygon.h
@@ -1,14 +1,14 @@
-﻿#ifndef POLYGON_H
-#define POLYGON_H
+﻿#ifndef POLYGON_H //POLYGON_H가 선언된적 없다면 아래 코드를 포함
+#define POLYGON_H //POLYGON_H 정의부
 
-class Polygon {
-	int size = 0;
-	int* xlist, * ylist;
-public:
-	Polygon(int size);
-	~Polygon();
-	void read();
-	bool get(int n, int& x, int& y);
+class Polygon { //폴리곤 클래스 생성
+	int size = 0; //다각형의 점의 개수 size 초기화
+	int* xlist, * ylist; //다각형들의 x값과 y값을 저장할 배열을 가리키는 포인터 선언
+public: //클래스 외부에서 접근 가능한 것들 정의
+	Polygon(int size); //점의 개수를 받는 생성자 함수 정의
+	~Polygon(); //할당 받은 동적 메모리 반환을 위한 소멸자 함수 정의
+	void read(); //점들의 좌표를 입력받는 함수 정의
+	bool get(int n, int& x, int& y); //n번째 점의 x값과 y값을 받는 함수 정의
 };
 
-#endif
+#endif //여기까지를 POLYGON_H으로 정의함.
