1c83ce64aef271a3fde750b875c89a6ab98f0ec9
Eve (Juhyeok Kim) <juhyeok.works@gmail.com>
2026-04-13 22:50:05 +0900

    디폴트 매개 변수 실습 2

---
+++ week07_defpara2/main.cpp
@@ -0,0 +1,19 @@
+﻿// 디폴트가 "", 1인 f함수를 만들어라
+
+#include <iostream>
+using namespace std;
+
+void f(char text = '\0', int line = 1) {
+	for (int i = 0; i < line; i++) {
+		for (int j = 0; j < 10; j++) 
+			cout << text;
+		cout << endl;
+	}
+	return;
+}
+
+int main() {
+	f(); // 한줄에 빈간을 10개 출력
+	f('%'); //1줄에 % 10개 출력 
+	f('@', 5); //5줄에 @ 10개 출력
+}
