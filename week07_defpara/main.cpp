cd0787e7d5499a5c3dbdc3ca431983849e44dc2d
Eve (Juhyeok Kim) <juhyeok.works@gmail.com>
2026-04-13 22:42:00 +0900

    디폴트 매개변수 실습

---
+++ week07_defpara/main.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+void star(int n=5) {
+	for (int i = 0; i < n; i++) cout << '*';
+	cout << endl;
+}
+
+void msg(int id, string text="") {
+	cout << id << text << endl;
+}
+int main() {
+	star(10);
+	star();
+
+	msg(10, "Hello");
+	msg(10);
+}
