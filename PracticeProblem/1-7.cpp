363ff2145e9eca182b537df01398b82f55686db5
Eve <evenlybigman@gmail.com>
2026-04-14 18:13:42 +0900

    1-7 추가

---
+++ PracticeProblem/1-7.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string cmpX, cmpY;
+	cout << "첫 번째 문자열을 입력하세요>>";
+	cin >> cmpX;
+	cout << "두 번째 문자열을 입력하세요>>";
+	cin >> cmpY;
+	if (cmpX == cmpY)
+		cout << "Yes";
+	else cout << "No";
+}
