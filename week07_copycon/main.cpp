f607eb969ddf135dbdf91e9e550dac86aedd5c8b
Eve <evenlybigman@gmail.com>
2026-04-14 15:32:28 +0900

    복사 생성자

--- week07_copycon/main.cpp
+++ week07_copycon/main.cpp
@@ -6,22 +6,22 @@ private:
 	int radius;
 public:
 	Circle() { radius = 1; }
-	Circle(int radius) { this->radius = radius; }
+	Circle(int radius) {
+		this->radius = radius; }
+	Circle(const Circle& src); // 디폴트 복사 생성자가 컴파일러에 의해 삽입되었다.
 	int getRadius() { return radius; }
 	double getArea() { return 3.14 * radius * radius; }
-	Circle(const Circle& c); //복사 생성자 선언부
-	// 디폴트 복사 생성자가 컴파일러에 의해 삽입되었다.
 };
 
+//디폴트 복사 생성자
 Circle::Circle(const Circle& c) {
-	this->radius = c.radius; 
+	this->radius = c.radius;
 }
 
 int main() {
-	Circle a;
 	Circle src(30);
 	Circle dest(src); //src랑 똑같은 dest라는 객체를 만들어줘
 
-	cout << src.getRadius() << endl;
-	cout << dest.getRadius() << endl;
+	cout << src.getArea() << endl;
+	cout << dest.getArea() << endl;
 }
