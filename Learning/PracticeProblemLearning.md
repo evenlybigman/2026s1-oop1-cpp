3c5dcbe176348725ca0cc0feed3161b4e249e495
Eve (Juhyeok Kim) <juhyeok.works@gmail.com>
2026-04-19 21:27:23 +0900

    폴더 위치 재구성 4-1 문제 풀이

---
+++ Learning/PracticeProblemLearning.md
@@ -0,0 +1,12 @@
+### Chap2
+
+cin.getline(char buf[], int size, char delimitChar) (공백까지 받을 수 있는 cin)
+
+strcmp 인수
+
+탭('\t')은 위치 정렬을 해준다
+
+### Chap4
+
+
+
---
+++ PracticeProblem/4-1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+
+//온도 5개를 저장할 배열을 동적 할당 받고 온도 5개를 입력받아 저장한다.
+//그리고 배열의 평균을 출력한뒤 동적 메모리를 반환한다.
+int main() {
+	double temperature;
+	double average = 0;
+	cout << "동적 할당 받을 메모리 개수: ";
+	cin >> temperature;
+	double* p = new double[temperature];
+	cout << temperature << "개의 온도 입력: ";
+	for (int i = 0; i < temperature; i++) {
+		cin >> p[i];
+		average += p[i];
+	}
+	average /= temperature;
+	cout << "평균은 " << average;
+	delete[] p;
+}
