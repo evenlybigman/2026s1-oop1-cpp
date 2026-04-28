2f6305649c6e63fbaa48e64cdb18d54d27610d4a
Eve (Juhyeok Kim) <juhyeok.works@gmail.com>
2026-04-28 23:50:30 +0900

    7-2-2 문제 풀이 추가

---
+++ PracticeProblem/7-2-1.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+
+class Coffee {
+	int water, espresso, sugar, cream;
+public:
+	Coffee(int water = 1, int coffee = 1, int sugar = 0, int cream = 0) {
+		this->water = water; this->espresso = coffee;
+		this->sugar = sugar; this->cream = cream;
+	}
+	void show() {
+		cout << "물 " << water << ", 커피 " << espresso;
+		cout << ", 설탕 " << sugar << ", 크림 " << cream << endl;
+	}
+	Coffee& operator += (Coffee op2);
+	Coffee& operator += (int op2);
+	Coffee operator ++(int x);
+};
+
+Coffee& Coffee::operator += (Coffee op2) {
+	water += op2.water; espresso += op2.espresso;
+	sugar += op2.sugar; cream += op2.cream;
+	return *this;
+}
+
+Coffee& Coffee::operator += (int op2) {
+	espresso += op2;
+	return *this;
+}
+
+Coffee Coffee::operator ++(int x) {
+	Coffee tmp = *this;
+	espresso++;
+	return tmp;
+}
+
+int main() {
+	Coffee black(2, 5, 0, 0), dabang(2, 2, 2, 2);
+	black += dabang;
+	black += 1;
+	black++;
+	black.show();
+}
