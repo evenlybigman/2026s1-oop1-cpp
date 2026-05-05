9b1133bd0f9ce0c36049a0ba6ca383e08ee02a70
Eve (Juhyeok Kim) <juhyeok.works@gmail.com>
2026-05-05 18:52:55 +0900

    접근 지정자 protected

---
+++ week10/protected/main.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+using namespace std;
+
+class Point {
+protected:
+	int x, y; //점의 좌표
+public:
+	void set(int x, int y) { this->x = x; this->y = y; }
+	void showPoint() { cout << "(" << x << "," << y << ")" << endl; }
+};
+
+class ColorPoint : public Point { // Point 클래스 상속. 파생 클래스
+	string color;
+public:
+	void setColor(string color) { this->color = color; }
+	void showColorPoint();
+	bool equals(ColorPoint p);
+};
+
+void ColorPoint::showColorPoint() {
+	showPoint();
+	cout << color << endl;
+}
+
+bool ColorPoint::equals(ColorPoint p) {
+	if (x == p.x && y == p.y && color == p.color)
+		return true;
+	else
+		return false;
+}
+
+int main() {
+	Point p;
+	p.set(2, 3);
+	p.showPoint();
+
+	ColorPoint cp;
+	cp.set(3, 4);
+	cp.setColor("red");
+	cp.showColorPoint();
+
+	ColorPoint cp2;
+	cp2.set(3, 4);
+	cp2.setColor("Red");
+	cout << ((cp.equals(cp2)) ? "true" : "false");
+}
