99ffe9763d7a64c054b5acb176a20149c84d3b15
Eve <evenlybigman@gmail.com>
2026-05-12 17:32:32 +0900

    11

---
+++ week11/GraphicEditor/main.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+using namespace std;
+
+class Shape {
+	Shape* next;
+protected:
+	virtual void draw() = 0;
+public:
+	Shape() { next = nullptr; }
+	void paint() {
+		draw();
+	}
+	Shape* add(Shape* p) {
+		next = p;
+		return p;
+	}
+	Shape* getNext() {
+		return next;
+	}
+};
+
+class Line : public Shape {
+protected:
+	virtual void draw() { cout << "Line" << endl; }
+};
+
+class Rect : public Shape {
+protected:
+	virtual void draw() { cout << "Rect" << endl; }
+};
+
+class Circle : public Shape {
+protected:
+	virtual void draw() { cout << "Circle" << endl; }
+};
+
+void drawALL(Shape *p) {
+	while (p != nullptr) {
+		p->paint();
+		p = p->getNext();
+	}
+}
+
+int main() {
+	Shape* pStart = nullptr;
+	Shape* pLast = nullptr;
+	pStart = new Circle();
+	pLast = pStart;
+
+	pLast = pLast->add(new Rect());
+	pLast = pLast->add(new Line());
+	pLast = pLast->add(new Circle());
+	drawALL(pStart);
+}
---
+++ week11/derived/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+class Base {
+public:
+	void draw() {
+		cout << "Base::f()" << endl;
+	}
+};
+
+class Derived : public Base {
+public:
+	virtual void draw() {
+		cout << "Derived::f()" << endl;
+	}
+};
+
+int main() {
+	Base b;
+	b.draw();
+	Derived d;
+	d.Base::draw();
+
+	Derived* pDer = &d;
+	pDer->draw();
+	Base* qBase = (Base*)&d;
+	qBase->draw();
+}
