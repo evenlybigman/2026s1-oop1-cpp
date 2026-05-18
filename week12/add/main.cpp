67e8dee3ea54cdb9cd54a0fb3bcbebe8982f8fe3
Eve <evenlybigman@gmail.com>
2026-05-18 18:33:16 +0900

    배열과 배열 길이를 받아 합을 리턴하는

---
+++ week12/add/main.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+template <class T>
+T add(const T array[], int size) {
+	T sum = 0;
+
+	for (int i = 0; i < size; i++) {
+		sum += array[i];
+	}
+
+	return sum;
+}
+
+int main() {
+	int x[] = { 1, 2, 3, 4, 5 };
+	double d[] = { 1.2, 2.3, 3.4, 4.5, 5.6, 6.7 };
+
+	cout << "sumt of x[] = " << add(x, 5) << endl;
+	cout << "sumt of d[] = " << add(d, 6) << endl;
+}
