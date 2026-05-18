c3ea83768cf3ef2960ae45739f6abe7953dcbfbc
Eve <evenlybigman@gmail.com>
2026-05-18 18:27:25 +0900

    두 값을 받아 더 큰 값을 리턴하는 제네릭 bigg

---
+++ week12/bigger/main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+template <typename T>
+T bigger(const T op1, const T op2) {
+	if (op1 > op2) return op1;
+	else return op2;
+}
+
+int main() {
+	int a = 20, b = 50;
+	char c = 'a', d = 'z';
+
+	cout << "bigger(20,50)의 결과는 " << bigger(a, b) << endl;
+	cout << "bigger('a','z')의 결과는 " << bigger(c, d) << endl;
+}
