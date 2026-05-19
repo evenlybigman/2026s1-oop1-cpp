d47f234be6587d1803b8b82a2df81ba5418564d7
Eve <evenlybigman@gmail.com>
2026-05-19 15:54:58 +0900

    템플릿

---
+++ week12/add2/main.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+template <class T>
+T bigger(const T& op1, const T& op2) {
+	if (op1 > op2) return op1;
+	else return op2;
+}
+
+
+int main() {
+	int a = 20, b = 50;
+	char c = 'a', d = 'z';
+	string e = "hello", f = "yoo";
+	cout << "bigger(20, 50)의 결과는 " << bigger(a, b) << endl;
+	cout << "bigger('a','z')의 결과는 " << bigger(c, d) << endl;
+	cout << "bigger('hello','yoo')의 결과는 " << bigger(string("hello"), string("yoo")) << endl;
+	cout << "bigger('hello','yoo')의 결과는 " << bigger(e, f) << endl;
+}
---
+++ week12/myswap2/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+template <typename T>
+void myswap(T& op1, T& op2) {
+	T tmp;
+	tmp = op1;
+	op1 = op2;
+	op2 = tmp;
+}
+
+int main() {
+	int a = 4, b = 5;
+	double c = 6.7, d = 8.9;
+	string x = "hello", y = "yoo";
+
+	myswap(a, b);
+	cout << a << ", " << b << endl;
+
+	myswap(c, d);
+	cout << c << ", " << d << endl;
+
+	myswap(x, y);
+	cout << x << ", " << y << endl;
+
+	return 0;
+}
