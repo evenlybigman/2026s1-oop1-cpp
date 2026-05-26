b01335df7359090d633c21913d181a332a7a7f29
Eve <evenlybigman@gmail.com>
2026-05-26 16:38:28 +0900

    getline 문제 실습

---
+++ week13/getline/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	while (true) {
+		cout << "암호>>";
+		char pw[80];
+		cin.getline(pw, 80, '\n');
+		if (strcmp(pw, "Good Morning") == 0) {
+			cout << "통과" << endl;
+			break;
+		}
+	}
+}
