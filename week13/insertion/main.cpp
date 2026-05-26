cc5445dad4eac368495fb4ae8e779752b7d0c658
Eve <evenlybigman@gmail.com>
2026-05-26 17:43:20 +0900

    조작자 함수

---
+++ week13/insertion/main.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+ostream& fivestar(ostream& out) {
+	cout << "*****";
+	return out;
+}
+
+ostream& beep(ostream& out) {
+	cout << '\a';
+	return out;
+}
+
+istream& question(istream& in) {
+	cout << "거울아 거울아 누가 제일 예쁘니?" << endl;
+	return in;
+}
+
+int main() {
+	string answer;
+	cin >> question >> answer;
+	cout << fivestar;
+	cout << beep;
+	cout << answer;
+}
