d954cd520c4ceb963bbbe5dd75d806b5f44f04e4
Eve <evenlybigman@gmail.com>
2026-05-26 15:47:56 +0900

    ostream 객체

---
+++ week13/cout/main.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "a";
+	cout.put('a');
+	cout.put(50); // 문자를 찍는 함수
+	cout.put('h').put('e').put('l');
+	cout.write("I love pro", 5);
+}
