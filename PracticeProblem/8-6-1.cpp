00cc89cca1640824929fc3d5ce2d05e1e2b22923
Eve (Juhyeok Kim) <juhyeok.works@gmail.com>
2026-05-31 14:49:28 +0900

    8-6-1 문제 풀이

---
+++ PracticeProblem/8-6-1.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+using namespace std;
+
+class Schedule {
+protected:
+	int count;
+	int* jobs;
+	int curLoc;
+public:
+	Schedule(int curloc, int n[], int count);
+	~Schedule();
+	int run();
+	int getJobs() { return count; }
+	int getCurLoc() { return curLoc; }
+};
+
+Schedule::Schedule(int curloc, int n[], int count) {
+	this->curLoc = curloc;
+	this->jobs = n;
+	this->count = count;
+	cout << "현재 위치는 " << curloc << "동: 배달 시작 ..." << curloc;
+	for (int i = 0; i < count; i++) {
+		cout << " -> " << jobs[i];
+	}
+	cout << endl;
+}
+
+int Schedule::run() {
+	int sum = 0;
+	for (int i = 0; i < count; i++) {
+		if (curLoc > jobs[i]) sum += curLoc - jobs[i];
+		else sum += jobs[i] - curLoc;
+		curLoc = jobs[i];
+	}
+	return sum;
+}
+
+Schedule::~Schedule() {
+
+}
+
+int main() {
+	int jobs[6];
+	cout << "택배를 픽업할 동을 요청 순서대로 6개 입력>>";
+	for (int i = 0; i < 6; i++)
+		cin >> jobs[i];
+	Schedule fcfs(10, jobs, 6);
+	cout << "전체 움직인 거리는 " << fcfs.run() << ", ";
+	cout << "현재 위치는 " << fcfs.getCurLoc() << "동" << endl;
+}
---
+++ PracticeProblem/8-6.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+class Schedule {
+protected:
+	int count;
+	int* jobs;
+	int curLoc;
+public:
+	Schedule(int curloc, int n[], int count);
+	~Schedule();
+	int run();
+	int getJobs() { return count; }
+	int getCurLoc() { return curLoc; }
+};
+
+
+
+int main() {
+	int jobs[6];
+	cout << "택배를 픽업할 동을 요청 순서대로 6개 입력>>";
+	for (int i = 0; i < 6; i++)
+		cin >> jobs[i];
+	Schedule fcfs(10, jobs, 6);
+	cout << "전체 움직인 거리는" << fcfs.run() << ", ";
+	cout << "현재 위치는 " << fcfs.getCurLoc() << "동" << endl;
+}
