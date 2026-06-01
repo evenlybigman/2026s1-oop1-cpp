4a5eee3fe64f449124fe2e27bf7e4c4dcb2e743f
Eve <evenlybigman@gmail.com>
2026-06-01 15:06:03 +0900

    9-3 문제 풀이

---
+++ PracticeProblem/9-2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+class Converter {
+private:
+	string srcUnit, destUnit;
+protected:
+	double ratio;
+	virtual double convert(double src) { return 0.0; }
+public:
+	Converter(string sUnit, string dUnit, double ratio) {
+		srcUnit = sUnit; destUnit = dUnit; this->ratio = ratio;
+	}
+	void run() {
+		cout << srcUnit << "을 " << destUnit << "로 바꿉니다. ";
+		cout << srcUnit << "을 입력하세요>>";
+		double src;
+		cin >> src;
+		cout << convert(src) << destUnit << "입니다." << endl;
+	}
+};
+
+class Inch2Centimeter : public Converter {
+public:
+	Inch2Centimeter(string sUnit, string dUnit, double ratio);
+	double convert(double src);
+};
+
+Inch2Centimeter::Inch2Centimeter(string sUnit, string dUnit, double ratio)
+	: Converter(sUnit, dUnit, ratio) {}
+
+double Inch2Centimeter::convert(double src) {
+	return src * ratio;
+}
+
+int main() {
+	Inch2Centimeter i2m("inch", "cm", 2.54);
+	i2m.run();
+}
