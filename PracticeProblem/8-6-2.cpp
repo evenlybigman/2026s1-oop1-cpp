dd6810d86ee32883a5683d8d431106ac5845bdbf
Eve (Juhyeok Kim) <juhyeok.works@gmail.com>
2026-06-03 03:46:26 +0900

    8-6-2 문제 풀이 추가(난이도 말도 안됨) 그리고 문제 오류 있는거 같다. main함수와 결과화면을 보면 생성자 함수에서 프로그램이 실행됨. 아마  run()에서 작동하는게 원래 의도 일듯함

--- PracticeProblem/8-6-2.cpp
+++ PracticeProblem/8-6-2.cpp
@@ -1,72 +1,110 @@
 ﻿#include <iostream>
+#include <cmath>
 using namespace std;
 
 class Schedule {
 protected:
 	int count;
-	int* jobs = NULL;
+	int* jobs;
 	int curLoc;
-	int totalDistance = 0;
 public:
 	Schedule(int curloc, int n[], int count);
 	~Schedule();
-	void run();
-	int getDistance() { return totalDistance; }
+	int run();
 	int getJobs() { return count; }
 	int getCurLoc() { return curLoc; }
 };
 
 Schedule::Schedule(int curloc, int n[], int count) {
-	this->curLoc = curloc;
-	this->jobs = new int[count];
-	this->count = count;
+	this->curLoc = curloc; //현재 동 위치
+	this->count = count; // n[] 크기
+	jobs = new int[count];
 
+	cout << "현재 위치는 " << curLoc << "동: 배달 시작 ..." << curLoc;
 	for (int i = 0; i < count; i++) {
+		cout << " -> ";
+		cout << n[i];
 		jobs[i] = n[i];
 	}
+	cout << endl;
 }
 
 Schedule::~Schedule() {
 	delete[] jobs;
 }
 
-void Schedule::run() {
-	cout << "현재 위치는 " << curLoc << "동: 배달 시작 ..." << curLoc;
+int Schedule::run() {
+	int sum = 0;
 	for (int i = 0; i < count; i++) {
-		cout << " -> " << jobs[i];
-	}
-	cout << endl;
-
-	for (int i = 0; i < count; i++) {
-		if (curLoc > jobs[i]) totalDistance += curLoc - jobs[i];
-		else totalDistance += jobs[i] - curLoc;
+		if (curLoc > jobs[i]) {
+			sum += curLoc - jobs[i];
+		}
+		else {
+			sum += jobs[i] - curLoc;
+		}
 		curLoc = jobs[i];
 	}
+	return sum;
 }
 
 class ShortestFirst : public Schedule {
 public:
 	ShortestFirst(int curloc, int n[], int count);
-	void run();
+	int run();
 };
 
-ShortestFirst::ShortestFirst(int curloc, int n[], int count)
-	: Schedule(curloc, n, count) {
-}
-
-void ShortestFirst::run() {
-	cout << "현재 위치는 " << curLoc << "동: 배달 시작 ..." << curLoc;
+ShortestFirst::ShortestFirst(int curloc, int n[], int count) 
+	:Schedule(curloc, n, count) {
+	bool* visited = new bool[count];
+	int tmpLoc = curLoc;
 	for (int i = 0; i < count; i++) {
-		cout << " -> " << jobs[i];
+		visited[i] = false;
+	}
+
+	cout << "현재 위치는 " << tmpLoc << "동: 배달 시작 ..." << tmpLoc;
+	for (int i = 0; i < count; i++) {
+		cout << " -> ";
+		
+		//가장 가까운 값 구하기
+		int minDistance = INT_MAX;
+		int minIdx = 0;
+		for (int j = 0; j < count; j++) {
+			if (visited[j] == true) continue;
+			if (tmpLoc > n[j]) {
+				if (minDistance > tmpLoc - n[j]) {
+					minIdx = j;
+					minDistance = tmpLoc - n[j];
+				}
+			}
+			else {
+				if (minDistance > n[j] - tmpLoc) {
+					minIdx = j;
+					minDistance = n[j] - tmpLoc;
+				}
+			}
+		}
+
+		cout << n[minIdx];
+		jobs[i] = n[minIdx];
+		tmpLoc = n[minIdx];
+		visited[minIdx] = true;
 	}
 	cout << endl;
+	delete[] visited;
+}
 
+int ShortestFirst::run() {
+	int sum = 0;
 	for (int i = 0; i < count; i++) {
-		for (int i = 0; jobs[i] != NULL; i++) { //10동 / [ 1 5 12 20 8 9 ] 
-			int* distance = new int[count - i];
-			if (curLoc > jobs[i]) distance[i] = 
+		if (curLoc > jobs[i]) {
+			sum += curLoc - jobs[i];
 		}
+		else {
+			sum += jobs[i] - curLoc;
+		}
+		curLoc = jobs[i];
 	}
+	return sum;
 }
 
 int main() {
@@ -76,12 +114,11 @@ int main() {
 		cin >> jobs[i];
 
 	Schedule fcfs(10, jobs, 6);
-	fcfs.run();
-	cout << "전체 움직인 거리는 " << fcfs.getDistance() << ", ";
+	cout << "전체 움직인 거리는 " << fcfs.run() << ", ";
 	cout << "현재 위치는 " << fcfs.getCurLoc() << "동" << endl;
 
 	ShortestFirst sf(10, jobs, 6);
-	sf.run();
-	cout << "전체 움직인 거리는 " << sf.getDistance() << ", ";
+	cout << "전체 움직인 거리는 " << sf.run() << ", ";
 	cout << "현재 위치는 " << sf.getCurLoc() << "동" << endl;
+
 }
