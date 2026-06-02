61316d23be996f7df4c005128946dd86ba0f8906
Eve <evenlybigman@gmail.com>
2026-06-02 16:41:06 +0900

    fin.get 함수를 이용한 파일 일기

---
+++ week14/fread/main.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+	ifstream fin;
+	fin.open("c:\\windows\\system.ini");
+
+	if (!fin) {
+		cout << "파일 열기 실패" << endl;
+		exit(1);
+	}
+
+	int ch;
+	int count = 0;
+	while ((ch = fin.get()) != EOF) {
+		cout.put(ch);
+		count++;
+	}
+	cout << endl;
+	cout << "파일은 " << count << "바이트 입니다." << endl;
+	fin.close();
+}
