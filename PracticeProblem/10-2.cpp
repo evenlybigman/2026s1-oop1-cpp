6ee8c8309b4ee2722dd39b25a8e620264457a6e1
Eve <evenlybigman@gmail.com>
2026-06-02 20:43:10 +0900

    10 - 2 문제 풀이 10 - 3 문제 풀이

---
+++ PracticeProblem/10-2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+template<typename T>
+void print(const T& start,const  T& end) {
+	T val = start;
+	for (val = start; val <= end; val++) {
+		cout << val << ' ';
+	}
+	cout << endl;
+}
+
+int main() {
+	print(500, 507);
+	print(3.5, 6.5);
+	print('b', 'y');
+	return 0;
+}
